a3c5bc6599882a60d48e0d49f0e4a6ca78827abb
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-03-28 15:02:37 +0300

    update app.js for lab05

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+// =========================================Lab: 04=========================================
+
 alert("Welcome to Engineering Guide Website!"); // this is introductory message
 var userName = prompt("Enter your name Please:"); // assign the user name to vaiable
 var userColor = prompt("Enter you favourite color:"); // assign a user color to variable
@@ -11,3 +13,32 @@ if(colorConfirm == true){  // to make sure the color is right
   document.write('<p style="background-color:' + userColor + '; font-size: 50px"> Welcome ' + userName + ' your color is ' + userColor + '</p>');
 }
 
+// =========================================Lab: 05=========================================
+
+var major = prompt("select you major",'electrical, civil, architectural, mechanical');
+
+while (major != "electrical" && major != "civil" && major != "architectural" && major != "mechanical"){ // maka sure that the major is right
+  major = prompt("Please enter the correct major!!")
+}
+
+var ImageNum = prompt("Enter the number of images you want from 1-5");
+
+while (ImageNum <= 0 || ImageNum >= 6){ // make sure that images in range
+   
+  ImageNum = prompt("Please enter from range 1-5")
+}
+
+console.log(ImageNum); // to check the number of images
+
+for(var i=1; i<=ImageNum; i++){ // to loop the images
+
+  if (major == "electrical"){
+    document.write('<img src="pic/electrical.jpg">');
+  }else if(major == "civil"){
+    document.write('<img src="pic/Civil.jpg">');
+  }else if(major == "architectural"){
+    document.write('<img src="pic/Architectural.jpg">');
+  }else if(major == "mechanical"){
+    document.write('<img src="pic/mechanical.jpg">');
+  }
+}
